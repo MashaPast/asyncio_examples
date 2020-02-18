27dbe5e6b9d32715b21cea69d3227f7defa69bf8
MashaPast <mariepast13@gmail.com>
2020-02-18 22:47:15 +0200

    WS client-server example is added

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 import asyncio
 import websockets
 
+
 async def hello(websocket, path):
     name = await websocket.recv()
     print(f"< {name}")
@@ -20,17 +21,3 @@ asyncio.get_event_loop().run_forever()
 # On the server side, websockets executes the handler coroutine hello once for each WebSocket connection. It closes the connection when the handler coroutine returns.
 
 
-# WS client example
-
-async def hello():
-    uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
-        name = input("What's your name? ")
-
-        await websocket.send(name)
-        print(f"> {name}")
-
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
-
-asyncio.get_event_loop().run_until_complete(hello())
